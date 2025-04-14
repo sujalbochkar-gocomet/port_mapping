6bb68d8284c5ccbe874b6bb10899b5ac170d9d76
sujalbochkar-gocomet <sujal.bochkar@gocomet.com>
2025-04-14 11:50:20 +0530

    Vercel Init

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
 });
